5d4918828147ed96e914a85a1a64970cf5484826
Kobayashi-Z3nk1 <abueva.carlos66@gmail.com>
2026-02-08 10:20:00 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,17 +3,20 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// ✅ Your Firebase Web App config (from your screenshot)
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS",
-  authDomain: "PASTE_YOURS",
-  projectId: "PASTE_YOURS",
-  storageBucket: "PASTE_YOURS",
-  messagingSenderId: "PASTE_YOURS",
-  appId: "PASTE_YOURS"
+  apiKey: "AIzaSyAjXo3Zx8P0ZQexAnmgEghI7Q9D5wy1IpQ",
+  authDomain: "school-based-task-tracker.firebaseapp.com",
+  projectId: "school-based-task-tracker",
+  storageBucket: "school-based-task-tracker.firebasestorage.app",
+  messagingSenderId: "940703711004",
+  appId: "1:940703711004:web:80082005103547740104d9",
+  measurementId: "G-MXXREFFY59"
 };
 
+// Initialize
 const app = initializeApp(firebaseConfig);
 
+// Export Auth + Firestore for other files to use
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
